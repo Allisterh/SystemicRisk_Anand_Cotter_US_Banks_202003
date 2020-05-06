6cbfb45a55e0baff6894e0dda363b1082e717809
abhinavananddwivedi <abhinavananddwivedi@gmail.com>
2020-05-07 05:20:14 +0530

    all tables and figures replicated!

--- US_Bank_SRE_panel_est_policy.R
+++ US_Bank_SRE_panel_est_policy.R
@@ -12,7 +12,7 @@ library(plm)
 library(poweRlaw)
 
 ### Source prior script for computing systematic risk exposures ### 
-name_script_file <- "US_Bank_SRE.R"
+name_script_file <- "US_Bank_SRE_results.R"
 source(name_script_file, echo = F) #Compute systematic risk exposure using daily price
 
 name_data_file <- 'CStat_Bank.dta'
@@ -179,9 +179,6 @@ panel_SRE_full_left <- SRE_US_banks_long_3 %>%
   dplyr::mutate('cusip_status' = purrr::map_dbl(cusip_8, func_cusip_check))
 
 
-cor_panel_variables <- cor(data.matrix(panel_SRE_full_left[, -c(1:8)]), 
-                           use = 'complete.obs')
-
 #############################################################################
 ################# Panel estimations begin here ##############################
 #############################################################################
@@ -196,13 +193,16 @@ panel_SRE_full <- panel_SRE_full_left %>%
 panel_SRE_full_2 <- panel_SRE_full %>%
   dplyr::distinct(cusip_8, Q_num, .keep_all = T)
 
+
 ### Main model specification ###
-formula_main <- SRE_2 ~ bank_size + debt_ratio + deposit_ratio + 
+
+formula_main <- SRE_2 ~ bank_size + deposit_ratio + com_eq_ratio +
   net_int_margin + npa_ratio + t1_t2_ratio + non_int_income_ratio +
-  debt_ratio_current + cash_div_ratio
+  cash_div_ratio + debt_ratio_current 
 
 func_panel_est <- function(formula = formula_main, 
                            panel_data = panel_SRE_full_2, 
+                           model_spec = 'within',
                            fixed_effect = 'twoways')
 {
   # This function accepts a formula, panel data matrix and model spec
@@ -213,7 +213,7 @@ func_panel_est <- function(formula = formula_main,
   # Panel estimation with fixed effects
   plm_fixed <- plm::plm(formula, 
                         data = panel_data, 
-                        model = "within",
+                        model = model_spec,
                         type = "HC0", 
                         effect = fixed_effect)
   
@@ -230,6 +230,34 @@ func_panel_est <- function(formula = formula_main,
   return(plm_out)
 }
 
+###########################################################################
+################# TABLE 6: Descriptive Stats ##############################
+###########################################################################
+
+func_summary <- function(vec)
+{
+  return(data.frame('Min' = min(vec),
+                    'Max' = max(vec),
+                    'Mean' = mean(vec),
+                    'Med' = median(vec),
+                    'SD' = sd(vec),
+                    'IQR' = IQR(vec)))
+}
+
+panel_variables <- panel_SRE_full_2 %>%
+  dplyr::select(SRE_2, bank_size, deposit_ratio, com_eq_ratio, 
+                net_int_margin, npa_ratio, t1_t2_ratio, 
+                non_int_income_ratio, cash_div_ratio, debt_ratio_current)
+
+print_table_6 <- apply(na.omit(panel_variables), 2, func_summary) %>% 
+  sapply(., rbind)
+
+###########################################################################
+################# TABLE 7: Correlation Matrix #############################
+###########################################################################
+
+print_table_7 <- cor(panel_variables, use = 'complete.obs')
+
 ################################
 ### Panel estimation results ###
 ################################
@@ -239,46 +267,29 @@ panel_est_main_model_full <- func_panel_est(formula = formula_main,
                                        panel_data = panel_SRE_full_2,
                                        fixed_effect = 'twoways')
 
+# Pooled
+panel_est_main_model_pool <- func_panel_est(model_spec = 'pooling')
+
 ## All banks pre 2006
 panel_est_main_model_H1 <- func_panel_est(formula = formula_main,
                                           panel_data = panel_SRE_full_2 %>%
                                             dplyr::filter(Q_num < max(Q_num)/2),
                                           fixed_effect = 'twoways')
+# Pooled pre 2006
+panel_est_main_model_H1_pool <- func_panel_est(panel_data = panel_SRE_full_2 %>%
+                                              dplyr::filter(Q_num < max(Q_num)/2),
+                                            model_spec = 'pooling')
 
 ## All banks post 2006
 panel_est_main_model_H2 <- func_panel_est(formula = formula_main,
                                        panel_data = panel_SRE_full_2 %>%
                                          dplyr::filter(Q_num >= max(Q_num)/2),
                                        fixed_effect = 'twoways')
+# Pooled post 2006
+panel_est_main_model_H2_pool <- func_panel_est(panel_data = panel_SRE_full_2 %>%
+                                                 dplyr::filter(Q_num >= max(Q_num)/2),
+                                               model_spec = 'pooling')
 
-### Subsample: Systemic banks ###
-
-# Systemic banks' cusips
-# bank_cusip_sys <- bank_cusip_file %>%
-#   dplyr::filter(Bank %in% banks_systemic) %>%
-#   dplyr::select(Bank, cusip_8)
-# 
-# cusip_8_sys <- bank_cusip_sys$cusip_8
-# 
-# ## Systemic banks full time
-# panel_est_main_model_sys <- func_panel_est(formula = formula_main,
-#                                        panel_data = panel_SRE_full_2 %>%
-#                                          dplyr::filter(cusip_8 %in% cusip_8_sys),
-#                                        fixed_effect = 'twoways')
-# 
-# ## Systemic banks pre 2006
-# panel_est_main_model_sys_H1 <- func_panel_est(formula = formula_main,
-#                                            panel_data = panel_SRE_full_2 %>%
-#                                              dplyr::filter(cusip_8 %in% cusip_8_sys &
-#                                                              Q_num < max(Q_num)/2),
-#                                            fixed_effect = 'twoways')
-# 
-# ## Systemic banks post 2006
-# panel_est_main_model_sys_H2 <- func_panel_est(formula = formula_main,
-#                                               panel_data = panel_SRE_full_2 %>%
-#                                                 dplyr::filter(cusip_8 %in% cusip_8_sys &
-#                                                                 Q_num >= max(Q_num)/2),
-#                                               fixed_effect = 'twoways')
 
 ### Subsample: Large banks (>$1B in 2019Q1) ###
 
@@ -294,22 +305,266 @@ panel_SRE_large_2019 <- panel_SRE_full_2 %>%
 panel_est_main_model_large <- func_panel_est(formula = formula_main,
                                              panel_data = panel_SRE_large_2019,
                                              fixed_effect = 'twoways')
+# Large banks pooled
+panel_est_main_model_large_pool <- func_panel_est(panel_data = panel_SRE_large_2019,
+                                                  model_spec = 'pooling')
 
 # Large banks pre 2006
 panel_est_main_model_large_H1 <- func_panel_est(formula = formula_main,
                                              panel_data = panel_SRE_large_2019 %>%
                                                dplyr::filter(Q_num < max(Q_num)/2),
                                              fixed_effect = 'twoways')
+# Large banks pre 2006 pooled
+panel_est_main_model_large_H1_pool <- func_panel_est(panel_data = panel_SRE_large_2019 %>%
+                                                  dplyr::filter(Q_num < max(Q_num)/2),
+                                                  model_spec = 'pooling')
 
 # Large banks post 2006
 panel_est_main_model_large_H2 <- func_panel_est(formula = formula_main,
                                                 panel_data = panel_SRE_large_2019 %>%
                                                   dplyr::filter(Q_num >= max(Q_num)/2),
                                                 fixed_effect = 'twoways')
+# Large banks post 2006 pooled
+panel_est_main_model_large_H2_pool <- func_panel_est(panel_data = panel_SRE_large_2019 %>%
+                                                       dplyr::filter(Q_num >= max(Q_num)/2),
+                                                     model_spec = 'pooling')
 
 
-formula_alt <- SRE_2 ~ bank_size + deposit_ratio + com_eq_ratio +
-  net_int_margin + npa_ratio + t1_t2_ratio + non_int_income_ratio +
-  cash_div_ratio + debt_ratio_current 
+#########################################################################
+############## Dodd-Frank Act Policy Effects ############################
+#########################################################################
 
-panel_est_alt <- func_panel_est(formula = formula_alt)
+bank_cusip_sys <- bank_cusip_file %>%
+  dplyr::filter(Bank %in% banks_systemic)
+
+### Tier 1 and 2 combined capital ratio ###
+
+# nest_SRE_t1t2 <- panel_SRE_full_2 %>%
+#   dplyr::select(Q_num, t1_t2_ratio) %>%
+#   dplyr::group_by(Q_num) %>%
+#   dplyr::arrange(Q_num) %>%
+#   tidyr::nest()
+
+nest_SRE_sys <- panel_SRE_full_2 %>%
+  dplyr::select(Q_num, cusip_8, t1_t2_ratio, com_eq_ratio) %>%
+  dplyr::filter(cusip_8 %in% bank_cusip_sys$cusip_8) %>%
+  dplyr::group_by(Q_num) %>%
+  dplyr::arrange(Q_num) %>%
+  tidyr::nest()
+
+
+nest_SRE_sys <- nest_SRE_sys %>%
+  dplyr::mutate('med_t1t2_sys' = purrr::map_dbl(data, 
+                                                function(tib){return(median(tib$t1_t2_ratio, 
+                                                                            na.rm = T))}),
+                'med_com_eq_sys' = purrr::map_dbl(data, 
+                                                  function(tib){return(median(tib$com_eq_ratio, 
+                                                                              na.rm = T))}))
+
+
+### Plotting the median systemic bank's ratios (Fig 6+7) ###
+x_breaks_sys <- seq(1, 108, by = 4)
+x_labels_sys <- paste0(seq(1993, 2019), "Q1")
+
+## T1 T2 combined
+plot_med_t1t2_sys <- ggplot(nest_SRE_sys, aes(Q_num, med_t1t2_sys)) +
+  geom_point() +
+  geom_line() +
+  geom_vline(xintercept = 71, linetype = 'dashed') +
+  geom_vline(xintercept = 63, linetype = 'dotdash') +
+#  ylim(11.5, 16) +
+  scale_x_continuous(breaks = x_breaks_sys,
+                     labels = x_labels_sys) +
+  labs(x = "", y = "Median systemic bank's Tier 1 and 2 ratio (combined, in %)") +
+  theme_bw() +
+  theme(text = element_text(size = 18)) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) 
+
+## Common equity ratio
+plot_med_com_eq_sys <- ggplot(nest_SRE_sys, aes(Q_num, med_com_eq_sys)) +
+  geom_point() +
+  geom_line() +
+  geom_vline(xintercept = 71, linetype = 'dashed') +
+  geom_vline(xintercept = 65, linetype = 'dotdash') +
+  scale_x_continuous(breaks = x_breaks_sys,
+                     labels = x_labels_sys) +
+  labs(x = "", y = "Median systemic bank's common equity ratio (in %)") +
+  theme_bw() +
+  theme(text = element_text(size = 20)) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) 
+
+###############################################################################
+######## TABLE 5: Mean variables before and after Dodd-Frank ##################
+###############################################################################
+
+SRE_sys_Dodd_Frank_pre <- panel_SRE_full_2 %>%
+  dplyr::select(Q_num, cusip_8, SRE_2, t1_t2_ratio, com_eq_ratio) %>%
+  dplyr::filter(cusip_8 %in% bank_cusip_sys$cusip_8) %>%
+  dplyr::filter(Q_num < 71)
+#  dplyr::filter(Q_num < 63)
+
+SRE_sys_Dodd_Frank_post <- panel_SRE_full_2 %>%
+  dplyr::select(Q_num, cusip_8, SRE_2, t1_t2_ratio, com_eq_ratio) %>%
+  dplyr::filter(cusip_8 %in% bank_cusip_sys$cusip_8) %>%
+  dplyr::filter(Q_num >= 71)
+#  dplyr::filter(Q_num >= 63)
+
+### Test of equality of pooled means ###
+
+## Tier 1 and 2 ratio
+# T test (parametric)
+mean_test_t1t2 <- t.test(SRE_sys_Dodd_Frank_pre$t1_t2_ratio,
+                         SRE_sys_Dodd_Frank_post$t1_t2_ratio,
+                         alternative = "less")
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_t1t2_wilcox <- wilcox.test(SRE_sys_Dodd_Frank_pre$t1_t2_ratio, 
+                                SRE_sys_Dodd_Frank_post$t1_t2_ratio, 
+                                alternative = "less")
+# KS test
+mean_test_t1t2_ks <- ks.test(SRE_sys_Dodd_Frank_pre$t1_t2_ratio, 
+                        SRE_sys_Dodd_Frank_post$t1_t2_ratio,
+                        alternative = "greater")
+
+## Common equity ratio
+# T test (parametric)
+mean_test_com_eq <- t.test(SRE_sys_Dodd_Frank_pre$com_eq_ratio,
+                         SRE_sys_Dodd_Frank_post$com_eq_ratio,
+                         alternative = "less")
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_com_eq_wilcox <- wilcox.test(SRE_sys_Dodd_Frank_pre$com_eq_ratio, 
+                                     SRE_sys_Dodd_Frank_post$com_eq_ratio, 
+                                     alternative = "less")
+# KS test
+mean_test_com_eq_ks <- ks.test(SRE_sys_Dodd_Frank_pre$com_eq_ratio, 
+                        SRE_sys_Dodd_Frank_post$com_eq_ratio,
+                        alternative = "greater")
+
+## SRE
+# T test (parametric)
+mean_test_SRE <- t.test(SRE_sys_Dodd_Frank_pre$SRE_2,
+                           SRE_sys_Dodd_Frank_post$SRE_2,
+                           alternative = "less")
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_SRE_wilcox <- wilcox.test(SRE_sys_Dodd_Frank_pre$SRE_2, 
+                                       SRE_sys_Dodd_Frank_post$SRE_2, 
+                                       alternative = "less")
+# KS test
+mean_test_SRE_ks <- ks.test(SRE_sys_Dodd_Frank_pre$SRE_2, 
+                               SRE_sys_Dodd_Frank_post$SRE_2,
+                               alternative = "greater")
+
+############################################################################
+############ Mean and variance tests during crises #########################
+############################################################################
+
+####### SRE ########
+
+panel_SRE_crises <- panel_SRE_full_2 %>%
+  dplyr::select(Q_num, SRE_2) %>%
+  dplyr::arrange(Q_num) %>%
+  dplyr::mutate('GR' = dplyr::case_when(Q_num %in% seq(60, 66) ~ 1,
+                                        TRUE ~ 0),
+                'EZ' = dplyr::case_when(Q_num %in% seq(70, 78) ~ 1,
+                                        TRUE ~ 0),
+                'Crises' = dplyr::case_when(GR == 1 | EZ == 1 ~ 1,
+                                            TRUE ~ 0))
+
+### The Great Recession ###
+
+SRE_GR <- panel_SRE_crises %>%
+  dplyr::filter(GR == 1)
+SRE_no_GR <- panel_SRE_crises %>%
+  dplyr::filter(GR == 0)
+
+# T test (parametric)
+mean_test_SRE_GR <- t.test(SRE_GR$SRE_2,
+                        SRE_no_GR$SRE_2,
+                        alternative = "greater")
+
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_SRE_GR_wilcox <- wilcox.test(SRE_GR$SRE_2,
+                                       SRE_no_GR$SRE_2,
+                                       alternative = "greater")
+# KS test
+mean_test_SRE_GR_ks <- ks.test(SRE_GR$SRE_2,
+                               SRE_no_GR$SRE_2,
+                               alternative = "less")
+
+### The Eurozone Crisis ###
+
+SRE_EZ <- panel_SRE_crises %>%
+  dplyr::filter(EZ == 1)
+SRE_no_EZ <- panel_SRE_crises %>%
+  dplyr::filter(EZ == 0)
+
+# T test (parametric)
+mean_test_SRE_EZ <- t.test(SRE_EZ$SRE_2,
+                           SRE_no_EZ$SRE_2,
+                           alternative = "greater")
+
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_SRE_EZ_wilcox <- wilcox.test(SRE_EZ$SRE_2,
+                                       SRE_no_EZ$SRE_2,
+                                       alternative = "greater")
+# KS test
+mean_test_SRE_EZ_ks <- ks.test(SRE_EZ$SRE_2,
+                               SRE_no_EZ$SRE_2,
+                               alternative = "less")
+
+
+### Crises ###
+
+SRE_GREZ <- panel_SRE_crises %>%
+  dplyr::filter(Crises == 1)
+SRE_no_GREZ <- panel_SRE_crises %>%
+  dplyr::filter(Crises == 0)
+
+# T test (parametric)
+mean_test_SRE_GREZ <- t.test(SRE_GREZ$SRE_2,
+                           SRE_no_GREZ$SRE_2,
+                           alternative = "greater")
+
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_SRE_GREZ_wilcox <- wilcox.test(SRE_GREZ$SRE_2,
+                                       SRE_no_GREZ$SRE_2,
+                                       alternative = "greater")
+# KS test
+mean_test_SRE_GREZ_ks <- ks.test(SRE_GREZ$SRE_2,
+                               SRE_no_GREZ$SRE_2,
+                               alternative = "less")
+
+
+####### Mean PC1 contribution ########
+
+
+######################################
+############ TABLE 4 #################
+######################################
+
+nest_share_crises <- nest_share %>%
+  dplyr::mutate('GR' = dplyr::case_when(Q_num %in% seq(60, 66) ~ 1,
+                                        TRUE ~ 0),
+                'EZ' = dplyr::case_when(Q_num %in% seq(70, 78) ~ 1,
+                                        TRUE ~ 0),
+                'Crises' = dplyr::case_when(GR == 1 | EZ == 1 ~ 1,
+                                            TRUE ~ 0))
+
+nest_share_GREZ <- nest_share_crises %>%
+  dplyr::filter(Crises == 1)
+nest_share_no_GREZ <- nest_share_crises %>%
+  dplyr::filter(Crises == 0)
+
+# T test (parametric)
+mean_test_share_GREZ <- t.test(nest_share_GREZ$eig_vec_1,
+                             nest_share_no_GREZ$eig_vec_1,
+                             alternative = "greater")
+
+# (Mann-Whitney) Wilcoxon test (nonparametric)
+mean_test_share_GREZ_wilcox <- wilcox.test(nest_share_GREZ$eig_vec_1,
+                                           nest_share_no_GREZ$eig_vec_1,
+                                           alternative = "greater")
+# KS test
+mean_test_share_GREZ_ks <- ks.test(nest_share_GREZ$eig_vec_1,
+                                   nest_share_no_GREZ$eig_vec_1,
+                                   alternative = "less")
+
