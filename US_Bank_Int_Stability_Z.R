74d69d132cb39b199cd7d069b57a6aa1f1473486
Your Nameabhinavananddwivedi <abhinavananddwivedi@gmail.com>
2021-03-18 18:39:41 +0530

    computing z scores, unbalanced panel posing problems

---
+++ US_Bank_Int_Stability_Z.R
@@ -0,0 +1,20 @@
+################################################################################
+########### US Bank Int: Bank stability forecasting [202101] with Z scores #####
+################################################################################
+
+### Load libraries ###
+library(tidyverse)
+library(moments)
+library(lmtest)
+library(sandwich)
+library(plm)
+library(tseries)
+library(Matrix)
+
+### Source prior script for computing systematic risk exposures ### 
+name_script_file <- "US_Bank_Int_Stability.R"
+source(name_script_file, echo = F) 
+
+########################################################################
+############ Quarterly Z score calculations for each bank ##############
+########################################################################
