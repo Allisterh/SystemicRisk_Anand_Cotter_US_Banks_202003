d1455c60d786086c5b66bc042a3b8c0490013517
Your Nameabhinavananddwivedi <abhinavananddwivedi@gmail.com>
2021-03-24 13:02:37 +0530

    Z score denominator computed

--- US_Bank_Int_Stability_Z.R
+++ US_Bank_Int_Stability_Z.R
@@ -19,9 +19,12 @@ source(name_script_file, echo = F)
 ############ Quarterly Z score calculations for each bank ##############
 ########################################################################
 
+# Computing Z score numerator = net income/total assets (=ROA) + 
+# common equity/total assets (common equity ratio/100)
 panel_Z <- panel_SRE_full_2 %>%
   dplyr::mutate('Z_num' = com_eq_ratio/100 + roa)
 
+# Minimum quarterly observations needed for computing Z denominator
 num_obs_Z <- 30
 
 panel_Z_nest <- panel_Z %>%
@@ -31,19 +34,28 @@ panel_Z_nest <- panel_Z %>%
   dplyr::mutate('nobs' = purrr::map_dbl(data, nrow)) %>%
   dplyr::filter(nobs >= num_obs_Z)
 
+# Function to calculate rolling standard deviation of ROA
 func_sd_roll <- function(df, window = 12)
 {
-  df_2 <- df %>% dplyr::arrange(., Q_num)
-  z_num <- c(rep(NA, nrow(df_2)))
-  q <- df_2$Q_num
+  # This function accepts a dataframe with first column as Quarter Numbers
+  # and second column as ROA; and returns a vector with rolling standard 
+  # deviations for ROA of length = window parameter
+  
+  df_2 <- df %>% dplyr::arrange(., Q_num) #Arrange in order of Q_num
+  z_num <- c(rep(NA, nrow(df_2))) #Initialize the numerator as vector of NA
+  q <- df_2$Q_num #Isolate quarter numbers
+  
   for (i in 1:(nrow(df_2)-window+1))
   {
+    # Isolate the part relevant for computing the rolling sd
     df_3 <- dplyr::filter(df_2, Q_num %in% q[i]:q[i+window-1])
-    z_num[i+window-1] = sd(df_3$roa, na.rm = T)
+    
+    z_num[i+window-1] = sd(df_3$roa, na.rm = T) #Compute rolling sd for ROA
   }
   
   return(z_num)
 }
 
+# Append the Z score denominator as a new column
 panel_Z_nest <- panel_Z_nest %>%
   dplyr::mutate('Z_den' = purrr:map(data, func_sd_roll))
