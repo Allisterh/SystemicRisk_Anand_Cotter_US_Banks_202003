d098389233fd1736e447c5d80238117679b7300a
Your Nameabhinavananddwivedi <abhinavananddwivedi@gmail.com>
2022-01-26 15:39:57 +0530

    new results compiled, including those during crises, high/low TED+VIX

--- US_Bank_Int_Beta.R
+++ US_Bank_Int_Beta.R
@@ -45,7 +45,27 @@ panel_data_beta <- panel_data_vol %>%
   dplyr::select(-c(date, siccd, ncusip, comnam, Year, Month, Quarter, datacqtr)) %>%
   dplyr::distinct(cusip_8, Q_num, .keep_all = TRUE)
 
+# Including crises
+panel_data_beta <- panel_data_beta %>%
+  dplyr::mutate('GR' = dplyr::case_when(Q_num %in% seq(59, 65) ~ 1,
+                                        TRUE ~ 0),
+                'EZ' = dplyr::case_when(Q_num %in% seq(69, 77) ~ 1,
+                                        TRUE ~ 0),
+                'LTCM' = dplyr::case_when(Q_num %in% seq(21, 23) ~ 1,
+                                          TRUE ~ 0), 
+                'Dotcom' = dplyr::case_when(Q_num %in% seq(37, 39) ~ 1, 
+                                            TRUE ~ 0),
+                'Crises' = dplyr::case_when(GR == 1 | EZ == 1 | LTCM == 1 | Dotcom == 1 ~ 1,
+                                            TRUE ~ 0))
+# Including bull-bear according to TED and VIX
+panel_data_beta <- panel_data_beta %>%
+  dplyr::left_join(., bull_bear_TED_VIX, by = 'Q_num')
+
 # Regression equation: 
+formula_beta_benchmark <- b_mkt ~ bank_size + t1_t2_ratio + npa_ratio + loss_prov_ratio
+formula_ivol_benchmark <- ivol ~ bank_size + t1_t2_ratio + npa_ratio + loss_prov_ratio
+formula_tvol_benchmark <- tvol ~ bank_size + t1_t2_ratio + npa_ratio + loss_prov_ratio
+
 formula_beta_int <- b_mkt ~ int_lag1 + int_lag2 + int_lag3 + int_lag4 + int_lag5 +
   bank_size + t1_t2_ratio + npa_ratio + loss_prov_ratio
 formula_ivol_int <- ivol ~ int_lag1 + int_lag2 + int_lag3 + int_lag4 + int_lag5 +
@@ -53,6 +73,10 @@ formula_ivol_int <- ivol ~ int_lag1 + int_lag2 + int_lag3 + int_lag4 + int_lag5 
 formula_tvol_int <- tvol ~ int_lag1 + int_lag2 + int_lag3 + int_lag4 + int_lag5 +
   bank_size + t1_t2_ratio + npa_ratio + loss_prov_ratio
 
+
+##########################################################################
+# Benchmark result: no intergation
+beta_int_panel_bench <- func_panel_est(formula_beta_benchmark, panel_data_beta)
 # Beta: all banks all time
 beta_int_panel_full <- func_panel_est(formula_beta_int, panel_data_beta)
 # Beta: large banks all time
@@ -65,9 +89,26 @@ beta_int_panel_pre_DF <- func_panel_est(formula_beta_int, dplyr::filter(panel_da
 # Beta: all banks post DF
 beta_int_panel_post_DF <- func_panel_est(formula_beta_int, dplyr::filter(panel_data_beta,
                                                                         Q_num >= 71))
+# Beta: Crises
+beta_int_panel_crises <- func_panel_est(formula_beta_int, dplyr::filter(panel_data_beta,
+                                                                        Crises == 1))
+# Beta: Bull TED
+beta_int_panel_H_TED <- func_panel_est(formula_beta_int, 
+                                            dplyr::filter(panel_data_beta, bull_bear_TED == 'H'))
+# Beta: Bear TED
+beta_int_panel_L_TED <- func_panel_est(formula_beta_int, 
+                                            dplyr::filter(panel_data_beta, bull_bear_TED == 'L'))
+# Beta: Bull VIX
+beta_int_panel_H_VIX <- func_panel_est(formula_beta_int, 
+                                          dplyr::filter(panel_data_beta, bull_bear_VIX == 'H'))
+# Beta: Bear VIX
+beta_int_panel_L_VIX <- func_panel_est(formula_beta_int, 
+                                          dplyr::filter(panel_data_beta, bull_bear_VIX == 'L'))
 
-#########################################
+###########################################################################
 
+# Benchmark result: no intergation
+ivol_int_panel_bench <- func_panel_est(formula_ivol_benchmark, panel_data_beta)
 # Ivol: all banks all time
 ivol_int_panel_full <- func_panel_est(formula_ivol_int, panel_data_beta)
 # Ivol: large banks all time
@@ -81,8 +122,25 @@ ivol_int_panel_pre_DF <- func_panel_est(formula_ivol_int, dplyr::filter(panel_da
 ivol_int_panel_post_DF <- func_panel_est(formula_ivol_int, dplyr::filter(panel_data_beta,
                                                                          Q_num >= 71))
 
-#######################################
+# Ivol: Crises
+ivol_int_panel_crises <- func_panel_est(formula_ivol_int, dplyr::filter(panel_data_beta,
+                                                                        Crises == 1))
+# ivol: Bull TED
+ivol_int_panel_H_TED <- func_panel_est(formula_ivol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_TED == 'H'))
+# ivol: Bear TED
+ivol_int_panel_L_TED <- func_panel_est(formula_ivol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_TED == 'L'))
+# ivol: Bull VIX
+ivol_int_panel_H_VIX <- func_panel_est(formula_ivol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_VIX == 'H'))
+# ivol: Bear VIX
+ivol_int_panel_L_VIX <- func_panel_est(formula_ivol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_VIX == 'L'))
 
+############################################################################
+# Benchmark result: no intergation
+tvol_int_panel_bench <- func_panel_est(formula_tvol_benchmark, panel_data_beta)
 # Tvol: all banks all time
 tvol_int_panel_full <- func_panel_est(formula_tvol_int, panel_data_beta)
 # Tvol: large banks all time
@@ -95,3 +153,21 @@ tvol_int_panel_pre_DF <- func_panel_est(formula_tvol_int, dplyr::filter(panel_da
 # Tvol: all banks post DF
 tvol_int_panel_post_DF <- func_panel_est(formula_tvol_int, dplyr::filter(panel_data_beta,
                                                                          Q_num >= 71))
+
+# Ivol: Crises
+tvol_int_panel_crises <- func_panel_est(formula_tvol_int, dplyr::filter(panel_data_beta,
+                                                                        Crises == 1))
+# ivol: Bull TED
+tvol_int_panel_H_TED <- func_panel_est(formula_tvol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_TED == 'H'))
+# ivol: Bear TED
+tvol_int_panel_L_TED <- func_panel_est(formula_tvol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_TED == 'L'))
+# ivol: Bull VIX
+tvol_int_panel_H_VIX <- func_panel_est(formula_tvol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_VIX == 'H'))
+# ivol: Bear VIX
+tvol_int_panel_L_VIX <- func_panel_est(formula_tvol_int, 
+                                       dplyr::filter(panel_data_beta, bull_bear_VIX == 'L'))
+
+############################################################################
